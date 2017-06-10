78e906d6c76fcb9b93285abeca3a50677943e2f2
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-06-10 19:31:07 +0300

    version #4

--- CSDemoProject/Program.cs
+++ CSDemoProject/Program.cs
@@ -10,6 +10,7 @@ namespace CSDemoProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //
             Console.ReadKey();
         }
     }
