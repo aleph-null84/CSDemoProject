48c3de8ea06c356e56b73c3b0aedf540aa01358b
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-06-10 19:14:04 +0300

    version #2

---
+++ CSDemoProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSDemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // dfsdfd
+            Console.ReadKey();
+        }
+    }
+}
